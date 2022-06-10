8bed28c1a09248abf240cd7cd8f76a3990f1edca
ds <ds@epxing.com>
2022-06-10 15:28:39 +0800

    易普云平台示例

---
+++ App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        {{count.a}}
+        {{count.b}}
+    </div>
+</template>
+
+<script>
+export default {
+  data() {
+      return {
+      count: {
+          a: 1,
+        }
+      }
+  },
+  created() {
+    setTimeout(() => {
+        this.count.a = 2
+        setTimeout(() => {
+            this.count.b = 2
+        }, 1000);
+    }, 2000);
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import frame, { createApp } from 'epx-frame'
+console.log(frame)
+
+createApp().$mount('#app')
